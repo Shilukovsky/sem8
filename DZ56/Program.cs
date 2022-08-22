363ed243c00cd8c713bb67991b90e8435e8ebc50
Nikolay <cccp_ussr@bk.ru>
2022-08-22 05:24:30 +0300

    added DZ58

--- DZ56/Program.cs
+++ DZ56/Program.cs
@@ -25,16 +25,6 @@ void PrintMatrix(int[,] array)
     }    
 }
 
-void PrintArray(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        if(i == 0) Console.Write("[");
-        if(i < array.Length - 1) Console.Write(array[i] + ",");
-        else Console.Write(array[i] + "]");
-    }
-}
-
 int SumLineNum(int[,] matrix, int i)
 {
   int sumLine = matrix[i,0];
